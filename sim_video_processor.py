8ca3c58274844590787a4b3b1665af0373f02b2c
Bailey Smith <warsplantresearch@gmail.com>
2024-07-31 12:44:33 +1200

    Added a config json file to initialize the hyperparameters

--- sim_video_processor.py
+++ sim_video_processor.py
@@ -1,54 +1,52 @@
 import cv2
 import math
+import argparse
+import json
+import numpy as np
 
-spray_system_enable = True # Just to entertain the sim.
+
+def load_config(config_file):
+    with open(config_file, 'r') as file:
+        return json.load(file)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Weed detection system.')
+    parser.add_argument('--config', type=str, default='config.json', help='Path to the configuration file')
+    return parser.parse_args()
+
+
+def initialize_video_capture(video_path):
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        raise IOError(f"Cannot open video file {video_path}")
+    return cap
+
+def process_frame(frame, config):
+    frame = frame[config['top_crop']:config['bottom_crop'], config['left_crop']:config['right_crop']]
+    color = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(color, np.array(config['dark_green']), np.array(config['light_green']))
+
+    dilated_connect = cv2.dilate(mask, None, iterations=3)
+    eroded = cv2.erode(dilated_connect, None, iterations=4)
+    dilated = cv2.dilate(eroded, None, iterations=5)
+    
+    return frame, dilated
+
 
 
 class VideoProcessor:
 
-    def __init__(self, cap):
+    def __init__(self, cap, config):
         self.cap = cap
-
-
-
-
-        self.weed_threshold = 8
-        self.dev_mode = True
-        self.weed_cam_status = 0
-
-        # cap.set(3, 480)
-        # cap.set(4,240)
-
-        FPS = 7 # Modifiable
-
-        # --------------------- COLOUR ---------------------
-        # Colour thresholds to identify weeds in dark soil
-        # ORIGINAL
-        # dark_green = (30, 50, 40) #HSV format (0, 0, 240) 
-        # light_green = (100,255, 255) #(255, 15, 255)
-
-
-        # dark_green = (40, 60, 80) #HSV format (0, 0, 240) 
-        # light_green = (100,150, 255) #(255, 15, 255)
-
-        # TEST
-        self.dark_green = (40, 65, 100) #HSV format (0, 0, 240) 
-        self.light_green = (80,255, 255) #(255, 15, 255)
+        self.config = config
 
         # --------------------------------------------------
 
-        self.left_crop = 100 #0
-        self.right_crop = 540 #640
-        self.top_crop = 0
-        self.bottom_crop =  400 # 210
-
         # Cropping parameters
-        self.screen_width = self.right_crop - self.left_crop
-        self.screen_height = self.top_crop - self.bottom_crop
+        self.screen_width = self.config['right_crop'] - self.config['left_crop']
+        self.screen_height = self.config['top_crop'] - self.config['bottom_crop']
 
         # Sprayer sections
-        self.spray_line_bottom = 20
-        self.spray_line_top = 130
         self.spray_line_optimal = 90
 
         self.send_slow_speed = 0
@@ -63,8 +61,6 @@ class VideoProcessor:
         self.xcord_deg_right = 0
         self.number_of_sprays_left = 0
         self.number_of_sprays_right = 0
-        self.x_centre_left = 90
-        self.x_centre_right = 140
         self.a = 0.99
         self.cX_old_left = 1000
         self.cX_old_right = 1000
@@ -86,17 +82,6 @@ class VideoProcessor:
         return False
 
 
-    def thresholding(self):
-
-        color = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(color, self.dark_green, self.light_green)
-        print("masked")
-
-        dilated_connect = cv2.dilate(mask, None, iterations=3 ) #Dilate to connect leaves
-        eroded = cv2.erode(dilated_connect, None, iterations=4) #Erode to remove noise
-        dilated = cv2.dilate(eroded, None, iterations=5 ) #Dilate to get correct size
-        return dilated
-
     def loop(self):
 
         while self.cap.isOpened():
@@ -114,51 +99,19 @@ class VideoProcessor:
         
             """2"""
 
-            self.frame = self.frame[self.top_crop:self.bottom_crop,self.left_crop:self.right_crop] #Cropping, should comment this out to check for sim
+            self.frame, self.grey_frame = process_frame(self.frame, self.config) #Cropping, should comment this out to check for sim
 
             print("framed")
 
-            if spray_system_enable:
+            if config['spray_system_enable']:
                 print("sprayer")
 
-                self.watchdog_frames += 1
                         
-                # if cluster_accum >= max_cluster_size and found_cluster == False:
-                #     found_cluster = True
-                #     #print("Found Cluster")            
-                            
-                # if found_cluster == True:
-                #     cluster_frames += 1
-                #     if cluster_frames <= 10*FPS:
-                #         send_slow_speed = 1 #Send a flag
-                #         #  print("Frame {}".format(cluster_frames))
-                        
-                #     else:
-                #         send_slow_speed = 0
-                #         cluster_frames = 0
-                #         found_cluster = False
-                    
-                # cluster_accum = 0    
-                
-
-
-                # if watchdog_frames >= 5*FPS and weed_found == 0:
-                #     sent_left[:] = []
-                #     sent_right[:] = []
-                #     watchdog_frames = 0
-                #     print("Timeout")
-                # elif weed_found == 1:
-                #     watchdog_frames = 0
-                #     weed_found = 0
-
-
-
-            self.grayscale_frame = self.thresholding()
 
             """6"""
 
             # Now that we have hopefully distinguished the coins, find and fit ellipses around the coins in the image.
-            contours, _ = cv2.findContours(self.grayscale_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours, _ = cv2.findContours(self.grey_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
             number_of_sprays = 0
             
@@ -179,25 +132,25 @@ class VideoProcessor:
                 cY = int(moments["m01"] / moments["m00"])
                 cv2.circle(self.frame, (cX, cY), 7, (0, 255, 0), -1) # Draw circles idenitified by moments
 
-                if self.spray_line_bottom  < cY < self.spray_line_top:
+                if self.config['spray_line_bottom']  < cY < self.config['spray_line_top']:
                     if area > 100:
                         number_of_sprays = 1
 
                     if cX < 240:
 
-                        if abs(self.cX_old_left - cX) > self.weed_threshold:
+                        if abs(self.cX_old_left - cX) > config['weed_threshold']:
                             print("cX: {}, cX_old_left:{}".format(cX,self.cX_old_left))
 
                             self.cX_old_left = cX
-                            xcord_deg_left = -math.degrees(math.atan(((cX-self.x_centre_left)*self.a)/150))
+                            xcord_deg_left = -math.degrees(math.atan(((cX-self.config['x_centre_left'])*self.a)/150))
                             xcord_deg_right = 0
                             number_of_sprays_left = number_of_sprays
                             number_of_sprays_right = 0
                     else: 
-                        if abs(self.cX_old_right - cX) > self.weed_threshold:
+                        if abs(self.cX_old_right - cX) > config['weed_threshold']:
                             print("cX: {}, cX_old_right:{}".format(cX,self.cX_old_right))
                             self.cX_old_right = cX
-                            xcord_deg_right = -math.degrees(math.atan(((cX-self.x_centre_right-240)*self.a)/150))
+                            xcord_deg_right = -math.degrees(math.atan(((cX-self.config['x_centre_right']-240)*self.a)/150))
                             xcord_deg_left = 0
                             number_of_sprays_right = number_of_sprays
                             number_of_sprays_left = 0
@@ -211,12 +164,11 @@ class VideoProcessor:
                     #     print("Sending this: ", input_array)
                     #     print("Area of contour: ", area)
             
-            cv2.line(self.frame, (0, self.spray_line_top), (self.screen_width, self.spray_line_top), (0, 255, 0), 2)
-            cv2.line(self.frame, (0, self.spray_line_bottom), (self.screen_width, self.spray_line_bottom), (0, 0, 255), 2)
+            cv2.line(self.frame, (0, self.config['spray_line_top']), (self.screen_width, self.config['spray_line_top']), (0, 255, 0), 2)
+            cv2.line(self.frame, (0, self.config['spray_line_bottom']), (self.screen_width, self.config['spray_line_bottom']), (0, 0, 255), 2)
 
             # Write_image(image_pub, frame)
-            if (self.dev_mode):
-                #print("in DEV")
+            if (self.config['dev_mode']):
                 
                 # Show images
                 cv2.namedWindow('Colour Segmentation', cv2.WINDOW_AUTOSIZE)
@@ -232,8 +184,8 @@ class VideoProcessor:
                 break
         
         else:
-            cv2.line(self.frame, (0, self.spray_line_top), (self.screen_width, self.spray_line_top), (0, 255, 0), 2)
-            cv2.line(self.frame, (0, self.spray_line_bottom), (self.screen_width, self.spray_line_bottom), (0, 0, 255), 2)
+            cv2.line(self.frame, (0, self.config['spray_line_top']), (self.screen_width, self.config['spray_line_top']), (0, 255, 0), 2)
+            cv2.line(self.frame, (0, self.config['spray_line_bottom']), (self.screen_width, self.config['spray_line_bottom']), (0, 0, 255), 2)
 
             # Write_image(image_pub, frame)
 
@@ -247,11 +199,13 @@ class VideoProcessor:
 
 if __name__ == "__main__":
 
-    # Get document folder. 
-    doc_folder = "test_content/"
+    # Get Program Configurations
+    args = parse_args()
+    config = load_config(args.config)
 
     # Get Video
-    cap = cv2.VideoCapture(doc_folder + "test_vid_1.avi")
+    cap = initialize_video_capture(config['doc_folder'] + config["video_file"])
     
-    vp = VideoProcessor(cap)
+    # Create and start the video processor.
+    vp = VideoProcessor(cap, config)
     vp.loop()
